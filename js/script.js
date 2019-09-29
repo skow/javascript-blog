307e7e7eaac837a982706d798f57051c0d85302e
Krzysztof Skowroński <studio@skovron.pl>
2019-09-29 11:28:45 +0200

    add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
 'use strict';
 
 const titleClickHandler = function(event){
+	
+	const clickedElement = this;
 	console.log('Link was clicked!');
-	// console.log(event);
+	console.log('clickedElement (width plus): ' + clickedElement);
 
-	/* remove class 'active' from all article links  */
+	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for(let activeLink of activeLinks){
 	  activeLink.classList.remove('active');
 	}
 
-	/* add class 'active' to the clicked link */
+	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll('.posts .active');
 	
 	for(let activeArticle of activeArticles) {
 		activeArticle.classList.remove('active');
 	}
 	
+	/* [DONE] add class 'active' to the clicked link */
+	clickedElement.classList.add('active');
 	
+	
+	
+	/* get 'href' attribute from the clicked link */
 
-/* remove class 'active' from all articles */
+	/* find the correct article using the selector (value of 'href' attribute) */
 
-/* get 'href' attribute from the clicked link */
-
-/* find the correct article using the selector (value of 'href' attribute) */
-
-/* add class 'active' to the correct article */
+	/* add class 'active' to the correct article */
 
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+	link.addEventListener('click', titleClickHandler);
 }
 
 
