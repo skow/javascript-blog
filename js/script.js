9261303ad9e2be51bfe01a842a4e975c1ec84441
Krzysztof Skowroński <studio@skovron.pl>
2019-11-08 12:42:46 +0100

    automatically generating links from posts

--- js/script.js
+++ js/script.js
@@ -35,10 +35,39 @@ const titleClickHandler = function(event){
 	showArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-	link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+	  optTitleSelector = '.post-title',
+	  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+	/* [DONE] remove contents of titleList */
+	const titleList = document.querySelector(optTitleListSelector);
+	titleList.innerHTML = '';
+	
+	/* [DONE] for each article */
+	const articles = document.querySelectorAll(optArticleSelector);
+	
+	for(let article of articles) {
+		/* [DONE] get the article id */
+		const articleId = article.getAttribute("id");
+		
+		/* [DONE] find the title element and get the title from the title element*/
+		const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+		
+		/* [DONE] create HTML of the link */
+		const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+		
+		/* [DONE] insert links into title list */
+		titleList.insertAdjacentHTML('beforeend', linkHTML);
+	}
+	const links = document.querySelectorAll('.titles a');
+	console.log(links);
+
+	for(let link of links){
+		link.addEventListener('click', titleClickHandler);
+	}
 }
 
-
+generateTitleLinks();
